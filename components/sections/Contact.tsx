05c79d6e54a4c4edbf6399d0df04a947f474be53
unknown <hasanabbas13@gmail.com>
2026-02-24 00:52:23 +0000

    address

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -23,7 +23,7 @@ export default function Contact() {
           <div className="space-y-2">
             <p>Email:</p>
             <p>Phone:</p>
-            <p>Office:</p>
+            <p>Office: 71-75 Shelton Street, Covent Garden, London, United Kingdom, WC2H</p>
           </div>
           <p className="text-white/70 text-sm">
             Confidential - Not for Public Consumption or Distribution
